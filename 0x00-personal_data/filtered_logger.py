2d22f1994a502a8ff8cf0abba21a49b81f91f7d2
Martin Mwenda <mwirigi.8kabush@gmail.com>
2024-11-07 17:30:03 +0000

    personal data

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for handling Personal Data
+"""
+
+import re
+from typing import List
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
+    """Filters a log line by obfuscating specified fields.
+    """
+    pattern = f"({'|'.join(fields)})=[^\\{separator}]*"
+    return re.sub(pattern, lambda x: f"{x.group(1)}={redaction}", message)
