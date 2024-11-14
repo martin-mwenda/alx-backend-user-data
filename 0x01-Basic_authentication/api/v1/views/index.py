5c622e343f2e495d91c9ddb0792003322d8652c4
Martin Mwenda <mwirigi.8kabush@gmail.com>
2024-11-14 03:48:33 +0000

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,12 @@ def unauthorized() -> None:
       - Raises a 401 error for unauthorized access
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Forbidden error.
+    """
+    abort(403)
