ed9832c782a2319ae74713c507b47ebb3e11c7ac
Martin Mwenda <mwirigi.8kabush@gmail.com>
2024-11-15 02:05:43 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -35,6 +35,34 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.before_request
+def authenticate_user():
+    """Checks if a user is authorized before processing a request.
+
+    For paths requiring authentication, validates the authorization header.
+    Raises a 401 error if the header is missing, or a 403 error if the user
+    cannot be authenticated. Skips authentication for paths
+    in `excluded_paths`.
+
+    Raises:
+        401: Missing authorization header for protected routes.
+        403: Unauthenticated user.
+    """
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
