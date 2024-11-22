2296bdeec463df1f3d4ec75a572be307eb9865ee
Martin Mwenda <mwirigi.8kabush@gmail.com>
2024-11-22 02:38:55 +0000

    flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+"""API Routes for Authentication Service"""
+from auth import Auth
+from flask import (Flask,
+                   jsonify,
+                   request,
+                   abort,
+                   redirect)
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def hello_world() -> str:
+    """Base route for authentication service API."""
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Registers a new user if it does not exist before.
+
+    Returns:
+        str: JSON response containing user email and creation status.
+
+    Raises:
+        KeyError: If required fields are missing.
+        ValueError: If the user already exists.
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
+
+
+@app.route('/sessions', methods=['POST'])
+def log_in() -> str:
+    """Logs in a user and returns session ID.
+
+    Args:
+        email (str): User's email.
+        password (str): User's password.
+
+    Returns:
+        str: JSON response with email and login message, and session ID
+        as a cookie.
+
+    Raises:
+        KeyError: If required fields are missing.
+        HTTPError 401: If the login is invalid.
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    msg = {"email": email, "message": "logged in"}
+    response = jsonify(msg)
+
+    response.set_cookie("session_id", session_id)
+
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def log_out() -> str:
+    """Logs out the user by destroying the session and redirecting to
+    base route.
+
+    Returns:
+        str: Redirect response to the base route if successful.
+
+    Raises:
+        HTTPError 403: If the session is invalid or the user does not exist.
+    """
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """Returns the user's profile if valid session, else 403 error.
+
+    Returns:
+        str: JSON payload with the user's email and HTTP status 200.
+        HTTPError 403: If the user is not authenticated.
+    """
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    msg = {"email": user.email}
+
+    return jsonify(msg), 200
+
+
+@app.route('/reset_password', methods=['POST'])
+def reset_password() -> str:
+    """Generates a reset password token for a registered email.
+
+    Returns:
+        str: JSON payload with the reset token and HTTP status 200.
+        HTTPError 403: If the email is not registered.
+    """
+    try:
+        email = request.form['email']
+    except KeyError:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    msg = {"email": email, "reset_token": reset_token}
+
+    return jsonify(msg), 200
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """Updates the password using a reset token.
+
+    Returns:
+        str: JSON response with email and success message if valid.
+        HTTPError 400: If required fields are missing.
+        HTTPError 403: If the reset token is invalid.
+    """
+    try:
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
+    except KeyError:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    msg = {"email": email, "message": "Password updated"}
+    return jsonify(msg), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
